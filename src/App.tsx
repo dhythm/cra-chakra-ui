73e325c825fcafe155062648a116553e31616e59
dhythm <yuta.okada.20@gmail.com>
2021-12-05 15:10:35 +0900

    implement Chakra example

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { Box, BoxProps } from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { Box, Code, Grid, Link, Text, VStack } from "@chakra-ui/react";
 import { FC } from "react";
-
-const MotionBox = motion<BoxProps>(Box);
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
 
 const App: FC = () => (
-  <MotionBox
-    boxSize="40px"
-    bg="red.300"
-    drag="x"
-    dragConstraints={{ left: -100, right: 100 }}
-    whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}
-  />
+  <Box textAlign="center" fontSize="xl">
+    <Grid minH="100vh" p={3}>
+      <ColorModeSwitcher justifySelf="flex-end" />
+      <VStack spacing={8}>
+        <Logo h="40vmin" pointerEvents="none" />
+        <Text>
+          Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+        </Text>
+        <Link
+          color="teal.500"
+          href="https://chakra-ui.com"
+          fontSize="2xl"
+          target="_blank"
+          rel="noopner noreferer"
+        >
+          Learn Chakra
+        </Link>
+      </VStack>
+    </Grid>
+  </Box>
 );
 
 export default App;
